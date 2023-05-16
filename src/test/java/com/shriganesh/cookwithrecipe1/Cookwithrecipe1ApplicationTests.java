3c5e139af90286e9c07366cd3d97bcaf70d6f61a
Am-I-Worthy <amiworthy05@gmail.com>
2023-05-16 09:15:06 +0530

    first commit

---
+++ src/test/java/com/shriganesh/cookwithrecipe1/Cookwithrecipe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shriganesh.cookwithrecipe1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cookwithrecipe1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
